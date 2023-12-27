ca153ec2593f17bfc733873e6891691648bef33a
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2023-12-27 20:37:31 +0800

    Refractor code

--- introduction/lib/pages/home.dart
+++ introduction/lib/pages/home.dart
@@ -7,47 +7,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
         ),
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-        leading: Container(
-          margin: const EdgeInsets.all(10),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: const Color(0xFFF7F8F8),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: SvgPicture.asset(
-            'arrow.svg',
-            height: 20,
-            width: 20,
-          ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      leading: button('arrow.svg', 20, 20),
+      actions: [
+        button('dots.svg', 5, 5),
+      ],
+    );
+  }
+
+  GestureDetector button(file, height, width) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        width: 37,
+        decoration: BoxDecoration(
+          color: const Color(0xFFF7F8F8),
+          borderRadius: BorderRadius.circular(10),
         ),
-        actions: [
-          Container(
-            margin: const EdgeInsets.all(10),
-            alignment: Alignment.center,
-            width: 37,
-            decoration: BoxDecoration(
-              color: const Color(0xFFF7F8F8),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: SvgPicture.asset(
-              'dots.svg',
-              height: 5,
-              width: 5,
-            ),
-          ),
-        ],
+        child: SvgPicture.asset(
+          file,
+          height: height,
+          width: width,
+        ),
       ),
     );
   }
