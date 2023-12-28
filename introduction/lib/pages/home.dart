d442aab166d7435c76ef28b6102105e42a6ba505
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2023-12-28 13:49:49 +0800

    Category title created

--- introduction/lib/pages/home.dart
+++ introduction/lib/pages/home.dart
@@ -10,8 +10,25 @@ class HomePage extends StatelessWidget {
       appBar: appBar(),
       backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           searchField(),
+          const SizedBox(height: 40),
+          const Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Text(
+                  'Category',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
