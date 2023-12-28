52edd04f7d940eceef6ad9ed3eead51713908534
DoughnutsNCookies <chuahtseyung2002@gmail.com>
2023-12-28 13:28:56 +0800

    Search bar added

--- introduction/lib/pages/home.dart
+++ introduction/lib/pages/home.dart
@@ -8,6 +8,32 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xFF1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0,
+                )
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                )
+              ),
+            ),
+          )
+        ]
+      ),
     );
   }
 
